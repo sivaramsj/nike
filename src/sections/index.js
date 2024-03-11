71e682d449fd638d1c53d0ec6bc24f9d11f5c717
sivaramsj <sivaramsj003@gmail.com>
2024-03-11 20:32:08 +0530

    online shoe app

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import Footer from "./Footer";
+import CustomerReviews from './CustomerReviews';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
